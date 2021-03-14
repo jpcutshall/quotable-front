7e6d64944f15abccf761703f95098484db53400b
jpcutshall <jpcutshall@gmail.com>
2021-03-14 18:06:57 -0400

    footer layout setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,23 @@ export default class Footer extends Component {
     render() {
         return (
             <div>
-                
+                <ul className="footer">
+                    <li>
+                        <a href="/">about</a>
+                    </li>
+                    <li>
+                        <a href="/">Terms of Service</a>
+                    </li>
+                    <li>
+                        <a href="/">Help</a>
+                    </li>
+                    <li>
+                        <a href="/">careers</a>
+                    </li>
+                    <li>
+                        <a href="/">developer info</a>
+                    </li>
+                </ul>
             </div>
         )
     }
